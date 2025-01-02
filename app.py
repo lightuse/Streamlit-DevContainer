1dd2623c8515524edd6635bc94affe30371bf0b1
lightuse <shinjiro.taji@gmail.com>
2025-01-03 00:51:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+html_string = '''
+<html>
+    <head>
+        <script>
+            alert("JavaScript is running!");
+        </script>
+    </head>
+    <body>
+        <h1>Hello from Streamlit!</h1>
+    </body>
+</html>
+'''
+components.html(html_string)
+
 
 st.title("My Streamlit App")
 st.write("Hello, World!")
